c9c47b280d9dcf3cfc22cd0048330ca1eacc2b0e
soklyhour <hoursokly011@gmail.com>
2024-05-03 16:16:46 -0500

    leave a message

--- image-search-app/index.js
+++ image-search-app/index.js
@@ -23,9 +23,11 @@ async function searchImages() {
   results.map((result) => {
     const imageWrapper = document.createElement("div");
     imageWrapper.classList.add("search-result");
+
     const image = document.createElement("img");
     image.src = result.urls.small;
     image.alt = result.alt_description;
+    
     const imageLink = document.createElement("a");
     imageLink.href = result.links.html;
     imageLink.target = "_blank";
